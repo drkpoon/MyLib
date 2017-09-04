932141bb99077c02e12ff77903e287cfc0af1a7f
drkpoon <drkpoon@hgcbroadband.com>
2017-09-05 06:28:37 +0800

    Change

--- app/src/main/java/com/user/derekpoon/mylib/MainActivity.java
+++ app/src/main/java/com/user/derekpoon/mylib/MainActivity.java
@@ -3,11 +3,14 @@ package com.user.derekpoon.mylib;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static com.user.derekpoon.trigolibrary.Compute.rectangleArea;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println(rectangleArea(3,5));
     }
 }
