decb2d0c471b444230fd6b2d3ce6a734162c211a
drkpoon <drkpoon@hgcbroadband.com>
2017-09-05 06:04:41 +0800

    First draft

---
+++ trigolibrary/src/main/java/com/user/derekpoon/trigolibrary/Compute.java
@@ -0,0 +1,11 @@
+package com.user.derekpoon.trigolibrary;
+
+/**
+ * Created by user on 5/9/2017.
+ */
+
+public class Compute {
+    public static int rectangleArea(int width, int height){
+        return width * height;
+    }
+}
